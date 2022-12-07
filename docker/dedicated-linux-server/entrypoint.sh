099eedd096ef70a885d54f302d778d6797a83fe5
Andrew Allbright <aallbrig@gmail.com>
2022-12-07 08:53:10 -0500

    Reverse proxy works for webGL

--- docker/dedicated-linux-server/entrypoint.sh
+++ docker/dedicated-linux-server/entrypoint.sh
@@ -1,9 +1,8 @@
 chmod +x /root/build/tanks.x86_64
-
 xvfb-run \
     --auto-servernum \
     --server-args='-screen 0 640x480x24:32' \
         /root/build/tanks.x86_64 \
             -batchmode \
             -nographics \
-            -logfile ~/server.log
+            -logfile /root/server.log
